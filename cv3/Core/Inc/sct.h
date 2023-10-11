473e966228ed55b5fd26cfb54feceabdbbb64aad
Dušan Kratochvíl <xkrato62@vutbr.cz>
2023-10-11 17:08:57 +0200

    cv3

---
+++ cv3/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: xkrato62
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+#endif /* INC_SCT_H_ */
